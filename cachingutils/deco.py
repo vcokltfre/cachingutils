29c68f6f15a2e65b7a0640caa1a119bb6c3098c5
vcokltfre <vcokltfre@gmail.com>
2022-01-17 05:48:15 +0000

    feat: add allow_unset

--- cachingutils/deco.py
+++ cachingutils/deco.py
@@ -16,23 +16,31 @@ def _extend_posargs(sig: list, posargs: list[int], *args) -> None:
         sig.append(hashed)
 
 
-def _extend_kwargs(sig: list, _kwargs: list[str], **kwargs) -> None:
+def _extend_kwargs(sig: list, _kwargs: list[str], allow_unset: bool = False, **kwargs) -> None:
     for name in _kwargs:
-        val = kwargs[name]
+        try:
+            val = kwargs[name]
+        except KeyError:
+            if allow_unset:
+                continue
+
+            raise
 
         hashed = hash(val)
 
         sig.append(hashed)
 
 
-def _get_sig(include_posargs: list[int] = None, include_kwargs: list[str] = None, *args, **kwargs) -> Sequence[int]:
+def _get_sig(
+    include_posargs: list[int] = None, include_kwargs: list[str] = None, allow_unset: bool = False, *args, **kwargs
+) -> Sequence[int]:
     signature = []
 
     if include_posargs:
         _extend_posargs(signature, include_posargs, *args)
 
     if include_kwargs:
-        _extend_kwargs(signature, include_kwargs, **kwargs)
+        _extend_kwargs(signature, include_kwargs, allow_unset, **kwargs)
 
     return tuple(signature)
 
@@ -41,6 +49,7 @@ def cached(
     timeout: float = None,
     include_posargs: list[int] = None,
     include_kwargs: list[str] = None,
+    allow_unset: bool = False,
     cache: Cache = None,
 ) -> Callable[[Callable[P, T]], Callable[P, T]]:
     _cache: Cache[Sequence[int], Any] = cache or Cache(timeout=timeout)
@@ -48,7 +57,7 @@ def cached(
     def decorator(func: Callable[P, T]) -> Callable[P, T]:
         @wraps(func)
         def wrapper(*args, **kwargs) -> T:
-            sig = _get_sig(include_posargs, include_kwargs, *args, **kwargs)
+            sig = _get_sig(include_posargs, include_kwargs, allow_unset, *args, **kwargs)
 
             if value := _cache.get(sig):
                 return value
@@ -68,6 +77,7 @@ def acached(
     timeout: float = None,
     include_posargs: list[int] = None,
     include_kwargs: list[str] = None,
+    allow_unset: bool = False,
     cache: Cache = None,
 ) -> Callable[[Callable[P, Awaitable[T]]], Callable[P, Awaitable[T]]]:
     _cache: Cache[Sequence[int], Any] = cache or Cache(timeout=timeout)
@@ -75,7 +85,7 @@ def acached(
     def decorator(func: Callable[P, Awaitable[T]]) -> Callable[P, Awaitable[T]]:
         @wraps(func)
         async def awrapper(*args, **kwargs) -> T:
-            sig = _get_sig(include_posargs, include_kwargs, *args, **kwargs)
+            sig = _get_sig(include_posargs, include_kwargs, allow_unset, *args, **kwargs)
 
             if value := _cache.get(sig):
                 return value
